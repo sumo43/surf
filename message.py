ae221a426424fb6fb2dd7a887efbc4abe18a0c68
yatsenkoa <yatsenkoa2020@gmail.com>
2021-10-04 22:20:02 -0600

    made exec of message

---
+++ message.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+
+
+# adapted from: 
+# https://github.com/a5482pick/Python-Chrome-App-Native-Messaging/blob/master/Host/native-messaging-example-host
+# this is a test
+
+
+import struct
+import sys
+
+
+def read_message():
+    while True:
+
+        text_length_bytes = sys.stdin.read(4)
+        if len(text_length_bytes) == 0:
+            print("text length is none")
+            sys.exit(0)
+
+        # Unpack message length as 4 byte integer
+        text_length = struct.unpack('i', text_length_bytes)[0]
+
+        # Read the text (JSON object) of the message.
+        text = sys.stdin.read(text_length).decode('utf-8')
+
+        print("text is", text)
+
+
+def Main():
+    read_message()
+
+
+if __name__ == "__main__":
+    Main()
+
+
+
