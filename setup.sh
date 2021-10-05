264bd5bc5641482028144cd3db8d6bef81f0a8bb
yatsenkoa <yatsenkoa2020@gmail.com>
2021-10-04 22:16:43 -0600

    made message file exec

--- setup.sh
+++ setup.sh
@@ -1,1 +1,5 @@
 cp native_messaging.json ~/Library/"Application Support"/Google/Chrome/NativeMessagingHosts/_com.surf.surf_search.json
+
+
+echo "installed"
+cat ~/Library/"Application Support"/Google/Chrome/NativeMessagingHosts/_com.surf.surf_search.json
